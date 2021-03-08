92b37e967c0936d8266270080b58bbb3fd260d16
Bunyamin Yigit <yigitbun@gmail.com>
2021-03-08 13:55:00 +0100

    ifstatement.php created

---
+++ ifstatement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Follow each link to the page example</h2>
+    <ul>
+        <li><a href="array.php">Simple Array and Printouts</a></li>
+        <li><a href="forloop.php">Simple Array and Printouts</a></li>
+        <li><a href="dowhileloop.php">Simple Array and Printouts</a></li>
+        <li><a href="whileloop.php">Simple Array and Printouts</a></li>
+        <li><a href="ifstatement.php">Simple Array and Printouts</a></li>
+        <li><a href="switchstatement.php">Simple Array and Printouts</a></li>
+    </ul>
+</body>
+</html>
