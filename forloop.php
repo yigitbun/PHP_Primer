c87d60407bafa1571764d910273d423d0c2ff677
Bunyamin Yigit <yigitbun@gmail.com>
2021-03-09 20:39:33 +0100

    en of for

--- forloop.php
+++ forloop.php
@@ -7,6 +7,20 @@
     <title>PHP Primer - FOR LOOP</title>
 </head>
 <body>
+    <h1>For Loops</h1>
     
+
+    <?php 
+        // For Loops
+        for($count = 0; $count < 10; $count++){
+            echo '<p>Hello World</p>';
+        }
+        for($count = 0; $count < 10; $count++){
+            echo "<p>The Count is: $count</p>";
+            // If we use " then we don't need a concatination
+        }
+
+        
+    ?>
 </body>
 </html>
