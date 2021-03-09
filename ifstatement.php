491f9ccfe65f369a3fd380912e66c015eecb5b63
Bunyamin Yigit <yigitbun@gmail.com>
2021-03-09 20:29:35 +0100

    en of switch

--- ifstatement.php
+++ ifstatement.php
@@ -12,11 +12,11 @@
     
         // An if statement that will carry out an action based on the value of the variable.
 
+        echo 'deneme';
         echo '<h2>If Else</h2>';
 
-        $grade = 70;
+        $grade = 30;
         
-
 
         if($grade >= 50){
             echo '<h3 style="color: green">You have passed</h3>';
@@ -30,17 +30,16 @@
         // If-Else If-Else
 
         if($grade == 'A'){
-            echo '<h2 style="color: green>YOU ARE A SUPERSTAR!</H2>';
+            echo '<h2 style="color: green">YOU ARE A SUPERSTAR!</h2>';
 
         }
         elseif($grade == 'B'){
-            echo '<h2 style="color: blue>YOU DID WELL!</h2>';
+            echo '<h2 style="color: blue">YOU DID WELL!</h2>';
         }
         else{
-            echo '<h2 style="color: red>YOU HAVE FAILED</h2>';
+            echo '<h2 style="color: red">YOU HAVE FAILED</h2>';
         }
 
-        echo 'deneme'
 
     ?>
 </body>
